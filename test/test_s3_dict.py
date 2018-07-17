0d8dbb396321ada5216d7d81a1bf393d955dc029
Marco Montagna <marcojoemontagna@gmail.com>
2018-07-17 07:59:25 -0700

    Fix boto mocks.

--- test/test_s3_dict.py
+++ test/test_s3_dict.py
@@ -10,24 +10,30 @@ from external_data_structures.dictionaries.s3 import S3Dict
 class TestS3Dict(unittest.TestCase):
 
   @mock_s3
-  def setUp(self):
+  def test_s3_dict_int_assignment_and_retrieval(self):
     self.bucket = S3Dict('test-4e1243')
     self.bucket.s3.create_bucket(Bucket='test-4e1243')
-
-  
-  def test_s3_dict_int_assignment_and_retrieval(self):
     self.bucket['asd'] = 123
     self.assertEqual(123, self.bucket['asd'])
 
+  @mock_s3
   def test_s3_dict_str_assignment_and_retrieval(self):
+    self.bucket = S3Dict('test-4e1243')
+    self.bucket.s3.create_bucket(Bucket='test-4e1243')
     self.bucket['msg'] = "hello world"
     self.assertEqual("hello world", self.bucket['msg'])
 
+  @mock_s3
   def test_s3_dict_list_assignment_and_retrieval(self):
+    self.bucket = S3Dict('test-4e1243')
+    self.bucket.s3.create_bucket(Bucket='test-4e1243')
     self.bucket['msg'] = [1,2, 3, "a", "b", "c"]
     self.assertEqual([1,2, 3, "a", "b", "c"], self.bucket['msg'])
 
+  @mock_s3
   def test_s3_dict_dict_assignment_and_retrieval(self):
+    self.bucket = S3Dict('test-4e1243')
+    self.bucket.s3.create_bucket(Bucket='test-4e1243')
     self.bucket['msg'] = {"a": None, "b": 2, "c" : "c"}
     self.assertEqual({"a": None, "b": 2, "c" : "c"}, self.bucket['msg'])
 
